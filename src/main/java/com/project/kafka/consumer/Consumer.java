47918d8df453560d1a74694e1927fb4f210ecaab
sahilappayev <sahilappayev@gmail.com>
2023-03-03 13:14:00 +0400

    Setted listener ack-mode to manual

--- src/main/java/com/project/kafka/consumer/Consumer.java
+++ src/main/java/com/project/kafka/consumer/Consumer.java
@@ -1,21 +1,40 @@
 package com.project.kafka.consumer;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class Consumer {
 
-    @KafkaListener(topics = "first_topic", groupId = "first_group", properties = {"auto.offset.reset:earliest"})
-    public void consumer(String message) {
-        log.info("Message from Kafka Consumer first_group : {}", message);
+    @KafkaListener(topics = "first_topic", groupId = "first_group",
+            properties = {ConsumerConfig.AUTO_OFFSET_RESET_CONFIG + ":earliest",
+                    ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG + ":false"})
+    public void consumer(@Payload String message,
+                         @Header(KafkaHeaders.ACKNOWLEDGMENT) Acknowledgment acknowledgment,
+                         @Header(KafkaHeaders.OFFSET) long offset,
+                         @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partitionId ) {
+        acknowledgment.acknowledge();
+        log.info("Message from Kafka Message: {}, partitionId : {}, offset: {}, acknowledgment: {}",
+                message, partitionId, offset, acknowledgment);
     }
 
-    @KafkaListener(topics = "first_topic", groupId = "second_group", properties = {"auto.offset.reset:earliest"})
-    public void consumer2(String message) {
-        log.info("Message from Kafka Consumer second_group: {}", message);
+    @KafkaListener(topics = "first_topic", groupId = "second_group",
+            properties = {ConsumerConfig.AUTO_OFFSET_RESET_CONFIG + ":earliest",
+                    ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG + ":false"})
+    public void consumer2(@Payload String message,
+                          @Header(KafkaHeaders.ACKNOWLEDGMENT) Acknowledgment acknowledgment,
+                          @Header(KafkaHeaders.OFFSET) long offset,
+                          @Header(KafkaHeaders.CONSUMER) KafkaConsumer<String, Object> consumer) {
+        log.info("Message from Kafka Message: {} Consumer : {}, partition: , offset: {}, acknowledgment: {}",
+                message, consumer, offset, acknowledgment);
     }
 
 
